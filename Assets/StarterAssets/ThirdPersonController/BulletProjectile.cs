98babbbfcb69b87ab5c07d4273169cd8173613e7
MIMUJSA <123391591+MIMUJSA@users.noreply.github.com>
2025-07-11 18:07:18 +0200

    3rd person and shooting that kind of works

---
+++ Assets/StarterAssets/ThirdPersonController/BulletProjectile.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    private Rigidbody bulletRigidbody;
+
+    private void Awake()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        float speed = 30f;
+        bulletRigidbody.linearVelocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
+
+   
